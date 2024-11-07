d044a236499a54cf807c7fba3d31ec202bfee495
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-11-07 06:50:31 -0800

    Update update.py

--- update.py
+++ update.py
@@ -70,7 +70,7 @@ if DATABASE_URL:
         environ["UPSTREAM_BRANCH"] = config_dict["UPSTREAM_BRANCH"]
     conn.close()
 
-UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
+UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/rizz-electrical-engg/aeon-vps.git")
 if len(UPSTREAM_REPO) == 0:
     UPSTREAM_REPO = ''
 
